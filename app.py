99dbc87065d2d8f146cf2d1091e90272efca52c5
adprimeganga-debug <adprimeganga@gmail.com>
2026-07-22 21:34:55 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -290,4 +290,5 @@ def horas_semanales():
 # ---------- MAIN ----------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
